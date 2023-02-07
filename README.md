f07563dcbbf9fdb6cc42d8da79af0eff997baf83
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-02-07 05:47:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flask-template
+# Flask-App
---
+++ my_app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, url_for, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+app = Flask(__name__)
+# Key for Forms
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+############################################
+
+        # SQL DATABASE AND MODELS
+
+##########################################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
